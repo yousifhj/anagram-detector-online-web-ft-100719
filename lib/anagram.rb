5963185895fc4dc96aa7299ffd838c6d347a29e7
Huda Yousif <hjdoski@gmail.com>
2019-10-17 03:07:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,8 @@ class Anagram
     @word = word
   end 
   
-  def match(words_aray)
-    words_array.select do |element|
+  def match(aray_of_words)
+    array_of_words.select do |element|
       (@word.split("").sort) == (element.split("").sort)
     end 
   end 
