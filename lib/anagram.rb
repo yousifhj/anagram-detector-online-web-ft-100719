a8730343fe732fce168a570016161fb2577462c1
Huda Yousif <hjdoski@gmail.com>
2019-10-17 03:11:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,3 +15,19 @@ class Anagram
   end 
   
 end 
+
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end
