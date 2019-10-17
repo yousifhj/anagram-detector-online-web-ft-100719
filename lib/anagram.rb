a48f112085a0c0933dc1ec245c3208ac530c6b03
Huda Yousif <hjdoski@gmail.com>
2019-10-17 03:04:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,5 @@ class Anagram
     @word = word
   end 
   
-  def matching(words_aray)
-    words_array.select do |element|
-      (@word.split("").sort) == (element.split("").sort)
-    end 
-  end 
+  
 end 
