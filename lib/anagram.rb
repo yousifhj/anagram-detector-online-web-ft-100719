1c1db9e5af3ff32f7d829b87507daa6d17b8c165
Huda Yousif <hjdoski@gmail.com>
2019-10-17 03:12:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,6 @@
 # Your code goes here!
 
-class Anagram 
-  
-  attr_accessor :word
-  
-  def initialize(word)
-    @word = word
-  end 
-  
-  def match(word_array)
-    word_array.select do |word|
-      word.split("") == @word.split("").sort 
-    end
-  end 
-  
-end 
+
 
 
 class Anagram
